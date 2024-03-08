00c74da6450578c4ff53a5013845f41acaea09bd
NHeroOP <neeravtheheroo@gmail.com>
2024-03-08 23:32:01 +0530

    Fix Code

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,20 @@ import AddExpense from "./Pages/Transactions/AddExpense"
 export default function App() {
   const dispatch = useDispatch()
   const expenseData = useSelector(state => state.expenseData.expData)
+  let income;
+  let expense;
 
-  if (expenseData.length > 0) {
+  try {
     const incomeData = expenseData.filter((data) => data?.isIncome && data?.price)
-    const income = incomeData.map((data) => Number(data?.price)).reduce((acc, curVal) => acc + curVal)
-  
+    income = incomeData.map((data) => Number(data?.price)).reduce((acc, curVal) => acc + curVal)
+    
     const expData = expenseData.filter((data) => !data?.isIncome && data?.price)
-    const expense = expData.map((data) => Number(data?.price)).reduce((acc, curVal) => acc + curVal)
-  
-    const final = income - expense
+    expense = expData.map((data) => Number(data?.price))?.reduce((acc, curVal) => acc + curVal)
+  } catch (error) {
+    
   }
+
+  const final = income - expense
 
 
 
@@ -35,17 +39,15 @@ export default function App() {
   }, []);
 
   useEffect(() => {
-    if (expenseData.length > 0) {
-      localStorage.setItem("expData", JSON.stringify(expenseData));
-    }
+    localStorage.setItem("expData", JSON.stringify(expenseData));
   }, [expenseData]);
 
   return (
     <div className=" max-w-full  flex flex-col justify-between px-4">
       <div className="flex gap-4  my-4 " >
         <Card type="profit" value={final > 0 ? final : "N/A"} title="Profit" />
-        <Card type="expense" value={expense} title="Expense" />
-        <Card type="income" value={income} title="Income" />
+        <Card type="expense" value={expense ? expense : "N/A"} title="Expense" />
+        <Card type="income" value={income ? income : "N/A"} title="Income" />
         <Card type="loss" value={final < 0 ? final : "N/A"} title="Loss" />
       </div>
       <div className="bg-white mb-4 mx-8 p-4 rounded-3xl">
